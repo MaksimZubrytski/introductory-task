4e8c17b251072cb1e52f94235ac848ef42b09ede
MaksimZubrytski <69052702+MaksimZubrytski@users.noreply.github.com>
2021-06-29 21:29:35 +0300

    feat: add watch gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,13 @@ const sourcemaps = require('gulp-sourcemaps');
 const watch = require('gulp-watch');
 
 gulp.task('sass-compile', function () {
-   return gulp.src('./sass/**/*.scss')
-   .pipe(sourcemaps.init())
-   .pipe(sass().on('error', sass.logError))
-   .pipe(sourcemaps.write('./'))
-   .pipe(gulp.dest('./dist/'));
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./sass/**/*.scss', gulp.series('sass-compile'))
 });
